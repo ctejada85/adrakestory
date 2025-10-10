3db687bb7c905e843d336dbd90753dcbeea6b01f
Carlos Tejada <carlostejada@gmail.com>
2025-10-10 13:52:08 -0400

    Remove VoxelWorld struct and its implementation to streamline resource management

--- src/systems/game/resources.rs
+++ src/systems/game/resources.rs
@@ -1,66 +1,7 @@
-use super::components::VoxelType;
 use bevy::prelude::*;
 use std::collections::HashMap;
 
-#[derive(Resource)]
-pub struct VoxelWorld {
-    pub width: i32,
-    pub height: i32,
-    pub depth: i32,
-    pub voxels: Vec<Vec<Vec<VoxelType>>>,
-}
-
-impl Default for VoxelWorld {
-    fn default() -> Self {
-        let width = 4;
-        let height = 3;
-        let depth = 4;
-
-        // Initialize with air
-        let mut voxels =
-            vec![vec![vec![VoxelType::Air; depth as usize]; height as usize]; width as usize];
-
-        // Create a floor layer (y=0) with grass
-        for x_voxels in voxels.iter_mut() {
-            for z in 0..depth as usize {
-                x_voxels[0][z] = VoxelType::Grass;
-            }
-        }
-
-        // Add voxels on top of the four corners
-        // Corners: (0,0), (0,3), (3,0), (3,3)
-        voxels[0][1][0] = VoxelType::Stone;
-        voxels[0][1][3] = VoxelType::Stone;
-        voxels[3][1][0] = VoxelType::Stone;
-        voxels[3][1][3] = VoxelType::Stone;
-
-        // Add some 1-sub-voxel-height platforms (step-up test)
-        voxels[1][1][1] = VoxelType::Dirt;
-        voxels[2][1][2] = VoxelType::Dirt;
-
-        // Add a voxel for stairs
-        voxels[2][1][1] = VoxelType::Stone;
-
-        Self {
-            width,
-            height,
-            depth,
-            voxels,
-        }
-    }
-}
-
-impl VoxelWorld {
-    pub fn get_voxel(&self, x: i32, y: i32, z: i32) -> Option<VoxelType> {
-        if x >= 0 && x < self.width && y >= 0 && y < self.height && z >= 0 && z < self.depth {
-            Some(self.voxels[x as usize][y as usize][z as usize])
-        } else {
-            None
-        }
-    }
-}
-
-pub const GRID_CELL_SIZE: f32 = 1.0; // Or adjust based on desired granularity
+pub const GRID_CELL_SIZE: f32 = 1.0;
 
 #[derive(Resource, Default)]
 pub struct SpatialGrid {
